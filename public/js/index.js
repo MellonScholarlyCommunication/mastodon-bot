1703e2f1c215459e8bd72d296df7725c1c5ea6ea
Patrick Hochstenbach <patrick.hochstenbach@ugent.be>
2024-08-07 10:20:10 +0200

    Updating javascript and style

--- public/js/index.js
+++ public/js/index.js
@@ -3,51 +3,92 @@ $(document).ready(function() {
 });
 
 function updateList() {
-    $.getJSON("accepted/", (data) => {
-        const members = data['contains'].sort(
-            function(a,b){ return b.localeCompare(a); }
-        );
+    $('.List').each( (i,el) => {
+        let watchUrl = $(el).attr('data-ref');
+        let implementations = [];
+        let maxLength = 10;
 
-        $('.List').empty();
-
-        for (let i = 0 ; i < members.length ; i++) {
-            const member = members[i];
-            $('.List').append(`<li><a href="${member}" class="toot">${member}</a></li>`); 
+        if (!watchUrl) {
+            return;
         }
 
-        $("a.toot").click(async function(e) {
-            e.preventDefault();
-            const href = $(this).attr('href');
-            const toot = await $.getJSON(href);
-            const content = toot['object']['content'].replace(/(<([^>]+)>)/ig, '');
-            const published = toot['published'];
-            const attributedTo = toot['actor']['id'];
-            const urls = toot['object']['url'];
+        if ($(el).attr('data-implement')) {
+            implementations = $(el).attr('data-implement').split(",");
+        }
 
-            const references = urls.map( (re) => {
-                return `<li><a href=\"${re.href}" class=\"reference">${re.href}</a></li>`;
-            }).join("\n");
+        if ($(el).attr('data-max')) {
+            maxLength = parseInt($(el).attr('data-max'));
+        }
 
-            let metadata = '<ul>';
-            for (let i = 0; i < urls.length ; i++) {
-                const met = await getMetdata(urls[i]['href']);
-                metadata += `<li class="Metadata">${JSON.stringify(met)}</li>`;
+        let dynamicClass = `${watchUrl}class`.replace(/[^a-zA-Z0-9]+/g,'_').replace(/_+/,'_');
+
+        $.getJSON(watchUrl, (data) => {
+            let members = data['contains'].sort(
+                function(a,b){ return b.localeCompare(a); }
+            );
+
+            members = members.slice(Math.max(members.length - maxLength, 0));
+
+            $(el).empty();
+
+            for (let i = 0 ; i < members.length ; i++) {
+                const member = members[i];
+                $(el).append(`<li><a href="${member}" class="${dynamicClass}">${member}</a></li>`); 
             }
 
-            $(".Details").html(`
-<div class="TootMeta"><tt>${published} - ${attributedTo}</tt></div>
-<div class="TootText">&gt;&gt;{text}</div>
-<div class="TootBody">${content}</div>
-<div class="TootText">&gt;&gt;{references}<ul>${references}</ul></div>
-<div class="TootText">&gt;&gt;{zotero}</div>
-${metadata}
-            `);
-            return false;
+            if (implementations.includes("toot")) {
+                updateTootClick(dynamicClass);
+            }
+            else if (implementations.includes("en")) {
+                updateENClick(dynamicClass);
+            }
+        }).fail(function(){
+            console.log(`Failed to retrieve ${watchUrl}`);
         });
-    }).fail(function(){
-        console.log("Failed to retrieve /accepted/");
     });
+
     setTimeout(updateList, 2000);
+}
+
+async function updateENClick(className) {
+    $(`a.${className}`).click(async function(e) {
+        e.preventDefault();
+        const href = $(this).attr('href');
+        const notification = await $.getJSON(href);
+        $(".Details").html(`<pre>${JSON.stringify(notification,null,2)}</pre>`);
+    });
+}
+
+async function updateTootClick(className) {
+    $(`a.${className}`).click(async function(e) {
+        e.preventDefault();
+        const href = $(this).attr('href');
+        const toot = await $.getJSON(href);
+        const content = toot['object']['content'].replace(/(<([^>]+)>)/ig, '');
+        const published = toot['published'];
+        const attributedTo = toot['actor']['id'];
+        const urls = toot['object']['url'];
+
+        const references = urls.map( (re) => {
+            return `<li><a href=\"${re.href}" class=\"reference">${re.href}</a></li>`;
+        }).join("\n");
+
+        let metadata = '<ul>';
+        for (let i = 0; i < urls.length ; i++) {
+            const met = await getMetdata(urls[i]['href']);
+            metadata += `<li class="Metadata">${JSON.stringify(met)}</li>`;
+        }
+
+        $(".Details").html(`
+            <div class="TootMeta"><tt>${published} - ${attributedTo}</tt></div>
+            <div class="TootText">&gt;&gt;{text}</div>
+            <div class="TootBody">${content}</div>
+            <div class="TootText">&gt;&gt;{references}<ul>${references}</ul></div>
+            <div class="TootText">&gt;&gt;{zotero}</div>
+            ${metadata}
+        `);
+                return false;
+    });
 }
 
 async function getMetdata(url) {
