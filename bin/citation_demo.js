284874356353cdb1123f474c0e41b827a84b87c5
Patrick Hochstenbach <patrick.hochstenbach@ugent.be>
2024-09-02 15:03:35 +0200

    Extending citation demo with style

--- bin/citation_demo.js
+++ bin/citation_demo.js
@@ -1,22 +1,27 @@
 #!/usr/bin/env node
 
+const { program } = require('commander');
+
 const path = require('path');
 const fs = require('fs');
 const Cite = require('citation-js');
 
 const file = process.argv[2];
 
-if (! file) {
-    console.error(`usage: ${path.basename(process.argv[1])} file`);
-    process.exit(1);
-}
+program
+  .name('citation_demo.js')
+  .option('-s,--style <style>', 'output style','apa')
+  .argument('<file>','input file')
+  .action( (file,opts) => {
+    let data = fs.readFileSync(file,{ encoding: 'utf8'});
+    let example = new Cite(data);
+    let output = example.format('bibliography', {
+        format: 'html',
+        template: opts.style,
+        lang: 'en-US'
+    });
+    
+    console.log(output);
+  });
 
-let data = fs.readFileSync(file,{ encoding: 'utf8'});
-let example = new Cite(data);
-let output = example.format('bibliography', {
-    format: 'html',
-    template: 'apa',
-    lang: 'en-US'
-});
-
-console.log(output);
+program.parse();
